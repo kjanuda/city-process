1d5273a2c1cef2d651d8f73deb7fb7e7aff67302
Januda J Kodithuwakku <113584734+kjanuda@users.noreply.github.com>
2025-11-17 23:18:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: ['http://localhost:3000', 'http://localhost:3001','https://urbanflowsl.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
 }));
